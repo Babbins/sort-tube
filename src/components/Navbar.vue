343d4d11a5cb3d1006ce9a91bf845607654e1697
Babbins <alimahmo@buffalo.edu>
2017-10-15 14:29:39 -0400

    Bootstraped vue and built nav

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    I am a nav
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'navbar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  position: fixed;
+  width: 100%;
+  top: 0;
+  height: 55px;
+  border: 2px red solid;
+  text-align: center;
+}
+</style>
