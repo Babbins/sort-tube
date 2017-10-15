0bedde5e7277e4ba13275c59e9c59809fdc1a751
Babbins <alimahmo@buffalo.edu>
2017-10-15 15:21:15 -0400

    Added Font, Logo, Logo CSS

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,8 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  $primary-color: #2c3e50;
+  font-family: 'Muli', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    I am a nav
+    <img id="logo" src="../assets/logo.png" alt="Sort Tube Logo" >
   </header>
 </template>
 
@@ -12,12 +12,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-header {
-  position: fixed;
-  width: 100%;
-  top: 0;
-  height: 55px;
-  border: 2px red solid;
-  text-align: center;
-}
+  header {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    height: 55px;
+    border-bottom: 2px red solid;
+    text-align: center;
+  }
+  #logo {
+    width: 30px;
+    position: absolute;
+    transform: translateY(50%);
+    left: 10px;
+  }
 </style>
