b068839b83c74f60d35ef727f10ae150bb0ef580
Babbins <alimahmo@buffalo.edu>
2017-10-20 21:23:12 -0400

    Getting video data from youtube

---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    {{ getData() }}
+  </header>
+</template>
+
+<script>
+import axios from 'axios'
+import {apiKey} from '../secrets.json'
+
+var apiURL = `https://www.googleapis.com/youtube/v3/search`
+
+export default {
+  name: 'Home',
+  data: function () {
+    return {
+      videos: []
+    }
+  },
+  methods: {
+    getData: function () {
+      axios.get(apiURL, {
+        params: {
+          key: apiKey,
+          part: 'snippet',
+          order: 'viewCount',
+          publishedAfter: '2017-10-15T20:19:55.413Z'
+        }
+      })
+      .then(function (response) {
+        console.log(response)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  header {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    height: 55px;
+    border-bottom: 2px red solid;
+    text-align: center;
+  }
+  #logo {
+    width: 30px;
+    position: absolute;
+    transform: translateY(50%);
+    left: 10px;
+  }
+</style>
